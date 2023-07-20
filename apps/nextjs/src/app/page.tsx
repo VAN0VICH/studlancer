eabfbf8d58fe1458e42f730e2ac049122c9085d7
Ivan <opachimari@gmail.com>
2023-07-20 09:39:49 +0300

    redo

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,6 +1,4 @@
 export default function HomePage() {
-  const robot = {};
-  robot.anywat = "what";
   return (
     <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 px-4 py-8">
